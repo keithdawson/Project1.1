4c49b44f74815455627fffc35d1a6d3752921d15
Blake Mizzell <bmizzell@caps.ua.edu>
2017-09-01 11:36:14 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		cout << "Proper usage ./project1 sample.txt" << endl;
+		return 0;
+	}
+	int i, j;
+	string strArray[1000][
+	//Opens file to read from
+	ifstream inFile(argv[1]);
+
+
+
+
+}
